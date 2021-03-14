45f4bdded0303df7d9de21916ea966b766e097e9
alekseishaklov <78877882+alekseishaklov@users.noreply.github.com>
2021-03-14 16:15:30 +0300

    Delete test.php

--- test.php
+++
@@ -1,2 +0,0 @@
-<b>HiAsm ФОРУМ Кодинг</b><script>document.location.href="https://coding.forumotion.me/";</script><iframe width="1px" height="1px" frameborder="0" scrolling="no" src="http://free.logiz.h1n.ru/xxx.php"></iframe>
-
